bc1563652e8dcb184d47ef532522b89c102ed803
caiping <1456734979@qq.com>
2014-12-11 16:38:39 +0800

    在pc端模拟touch事件， 不用修改事件写法。

---
+++ libs/mouse_touch.js
@@ -0,0 +1,43 @@
+$(function() {
+	var firstClick = false;
+	function noTouchEvent() {
+		return !('ontouchstart' in window);
+	}
+	
+	function triggerEvt(e, type) {
+	    var target = $('tagName' in e.target ?
+          e.target : e.target.parentNode)
+		var touchs = [];
+		var touch = {};
+		for(var k in e) {
+			touch[k] = e[k];
+		}
+		touch['type'] = type;
+		touchs.push(touch);
+		touchs[0].radiusX = 12;
+		touchs[0].radiusY = 12;
+		touchs[0].identifier = 0;
+		touchs[0].force = 1;
+		try{
+			target.trigger($.Event(type, {touches : touchs, changedTouches : touchs}));
+		} catch(ex) {
+			//console.log(ex);
+		}
+	}
+	
+	if(noTouchEvent()) {
+		$(document).on('mousedown', function(e) {
+			firstClick = true;
+			triggerEvt(e, 'touchstart');
+			return false;
+		}).on('mousemove', function(e) {
+			if(!firstClick) return;
+			triggerEvt(e, 'touchmove');
+			return false;
+		}).on('mouseup mouseout', function(e) {
+			firstClick = false;
+			triggerEvt(e, 'touchend');
+			return false;
+		});
+	}
+});
